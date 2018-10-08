104d902f5b1b34f65470c682f6d3918c07856141
akunim <akunim.draws@gmail.com>
2018-10-08 16:24:47 +0000

    Bot no longer claps at messages containing only exclamation marks

--- bot.js
+++ bot.js
@@ -1,22 +1,23 @@
 const Discord = require('discord.js');
 const RichEmbed = require('discord.js').RichEmbed;
 const client = new Discord.Client();
+const rp = require('request-promise');
 
 const tumblrFetcher = require('./TumblrFetcher.js');
 const twitterFetcher = require('./TwitterFetcher.js');
 
-var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-var urlregex = new RegExp(expression);
+var urlRegex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
 
 const secrets = require("./secrets.js");
 const BOT_KEY = secrets.BOT_KEY();
+
+const COMMAND_SIGNS = ["!", "/"];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-
   // DON'T TALK TO YOURSELF
   if (msg.author.id !== client.user.id) {
     
@@ -32,20 +33,39 @@ client.on('message', msg => {
     }
     
     // URL ACTIONS
-    if (msg.content.match(urlregex)) {
-      var url = urlregex.exec(msg.content)[0];
-      if (url.includes("tumblr") && !url.includes("media") && url.includes("post")) {
-        tumblrFetcher.fetch(url, msg);
+    if (msg.content.match(urlRegex)) {
+      var url = urlRegex.exec(msg.content)[0];
+      
+      // Check if you're supposed to be ignoring this link
+      if (msg.content.charAt(msg.content.indexOf(url)-1) != '<'){
+        if (url.includes("tumblr") && !url.includes("media") && url.includes("post")) {
+          tumblrFetcher.fetch(url, msg);
+        }
+        if (url.includes("twitter")) {
+          twitterFetcher.fetch(url, msg);
+        }        
       }
-      if (url.includes("twitter")) {
-        twitterFetcher.fetch(url, msg);
+      else {
+        msg.channel.send("🤐");
       }
     }
-  }
+    
+    // COMMANDS
+    if(COMMAND_SIGNS.indexOf(msg.content.substring(0,1)) > -1){
+      var command = msg.content.split(" ")[0].substring(1);
+      var params = msg.content.split(" ");
+      params.shift();
+      console.log("COMMAND: " + command + " " + params);
+      var commandRegex = new RegExp(/!+/gm);
+      if (command !== "" && !command.match(commandRegex)){
+        msg.channel.send("👏"); 
+      }
+    }
+  } // END don't talk to self
 });
 
 client.on('error', (err) => {
-  console.log("WEBSOCKET ERROR: " + err);
+  console.log("WEBSOCKET ERROR: " + err.message);
 });
 
 console.log("Loggin' in...");
